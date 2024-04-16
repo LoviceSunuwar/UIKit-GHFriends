aa6e13447fda9b40f243c3eb2cef91d7a72d0e7a
Lovice Sunuwar <53461002+LoviceSunuwar@users.noreply.github.com>
2024-04-15 21:21:48 -0400

    Here is the base of the project where we have removed the storybaord from the project since we are going to make the views programatically.

---
+++ GHFriends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFriends
+//
+//  Created by Lovice Sunuwar on 15/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
