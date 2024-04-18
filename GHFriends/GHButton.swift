f85e39c05ec69cd7dbfeed017edb42cef1b6c932
Lovice Sunuwar <53461002+LoviceSunuwar@users.noreply.github.com>
2024-04-18 00:41:13 -0400

    creating cusotm buttons and textfields for the app. configuring the button on the same file cause, we dont want to repeat the same exact thing everytme we want to use a button or a textfiled over and over again. translatesAutoresizingMaskIntoConstraints = false This is saying programatiically that hey, man i will be using autolayout

---
+++ GHFriends/GHButton.swift
@@ -0,0 +1,20 @@
+//
+//  GHButton.swift
+//  GHFriends
+//
+//  Created by Lovice Sunuwar on 18/04/2024.
+//
+
+import UIKit
+
+class GHButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ GHFriends/GHTextField.swift
@@ -0,0 +1,20 @@
+//
+//  GHTextField.swift
+//  GHFriends
+//
+//  Created by Lovice Sunuwar on 18/04/2024.
+//
+
+import UIKit
+
+class GHTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
