71da28ba3caf19d64e6fda8a7d45eac29d9e63ec
Lovice Sunuwar <53461002+LoviceSunuwar@users.noreply.github.com>
2024-12-05 20:41:14 -0500

    Testing the ScreenGuard package for Screenshot overlay View

--- GHFriends/SceneDelegate.swift
+++ GHFriends/SceneDelegate.swift
@@ -1,95 +1,99 @@
-//
-//  SceneDelegate.swift
-//  GHFriends
-//
-//  Created by Lovice Sunuwar on 15/04/2024.
-//
-
 import UIKit
+import ScreenGuard // Import the ScreenGuard package
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
-
+    var screenshotViewModel: ScreenshotViewModel!
+    var dismissableOverlay: ScreenshotOverlayView? // Use the custom overlay from the package
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-//        let searchNC = UINavigationController(rootViewController: SearchVC()) // Did this initially before creating a funciton for oth of them
-//        let favouritesNC = UINavigationController(rootViewController: FavouritesListVC())
-        
-//        let tabbar = UITabBarController()
-//        tabbar.viewControllers = [searchNC, favouritesNC] // Did this intially before creating createTabBar function ## Refactoring
-        window = UIWindow(frame: windowScene.coordinateSpace.bounds) // Full Screen
+
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        // Root View Controller
         window?.rootViewController = createTabbar()
-//        window?.rootViewController = tabbar // Since our tab bar controller is going to hold navigation view controller which will hld the view controller Did this intially ebfore creating createTababr
-        window?.makeKeyAndVisible() // show it
-        
-        configureNavigationBar()
-    }
-    
-    func createSearchNC() -> UINavigationController {
-        let searchVC = SearchVC()
-        searchVC.title = "Search"
-        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0) // tag means where its0 cause its first index we want on the tab bar controller
-        
-        return UINavigationController(rootViewController: searchVC)
+        window?.makeKeyAndVisible()
+
+        // Initialize ScreenshotViewModel
+        screenshotViewModel = ScreenshotViewModel()
+
+        // Start observing for screenshots
+        screenshotViewModel.startObserving()
+
+        // Set up callback to show overlay when a screenshot is detected
+        screenshotViewModel.onScreenshotDetected = { [weak self] in
+            DispatchQueue.main.async {
+                self?.showCustomBottomView() // Show at the bottom
+            }
+        }
     }
 
-    func createFavouritesNC() -> UINavigationController {
-        let favouritesVC = FavouritesListVC()
-        favouritesVC.title = "Favourites"
-        favouritesVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
-        
-        return UINavigationController(rootViewController: favouritesVC)
+    // Function to show the custom bottom view
+    func showCustomBottomView() {
+        guard let window = window else { return }
+
+        // Create the custom bottom view
+        let bottomView = UIView()
+        bottomView.backgroundColor = .lightGray
+        bottomView.layer.cornerRadius = 10
+        bottomView.clipsToBounds = true
+        bottomView.frame = CGRect(x: 0, y: 0, width: window.bounds.width, height: window.bounds.height * 0.3) // Only height matters now
+
+        // Add buttons to the bottom view
+        let buttonWidth = bottomView.bounds.width * 0.2
+        let buttonHeight = bottomView.bounds.height * 0.6
+        let buttonSpacing = bottomView.bounds.width * 0.05
+
+        for i in 0..<4 {
+            let button = UIButton(type: .system)
+            button.frame = CGRect(x: CGFloat(i) * (buttonWidth + buttonSpacing) + buttonSpacing,
+                                  y: (bottomView.bounds.height - buttonHeight) / 2,
+                                  width: buttonWidth,
+                                  height: buttonHeight)
+            button.setTitle("Button \(i + 1)", for: .normal)
+            button.backgroundColor = .blue
+            button.layer.cornerRadius = 10
+            button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+            bottomView.addSubview(button)
+        }
+
+        // Use ScreenshotOverlayView with the `bottom` position
+        let overlayView = ScreenshotOverlayView(frame: window.bounds, customView: bottomView, position: .bottom)
+        window.addSubview(overlayView)
+
+        // Store the overlay view reference
+        self.dismissableOverlay = overlayView
     }
-    
+
+    // Function to handle button tap
+    @objc func buttonTapped(_ sender: UIButton) {
+        if let title = sender.titleLabel?.text {
+            print("\(title) pressed!")
+        }
+    }
+
+    // Function to create the tab bar controller
     func createTabbar() -> UITabBarController {
         let tabbar = UITabBarController()
         UITabBar.appearance().backgroundColor = .systemBackground
         UITabBar.appearance().tintColor = .systemGreen
         tabbar.viewControllers = [createSearchNC(), createFavouritesNC()]
-    
         return tabbar
     }
-    
-    func configureNavigationBar() {
-        UINavigationBar.appearance().tintColor = .systemGreen
-    }
-    
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+
+    // Function to create the search navigation controller
+    func createSearchNC() -> UINavigationController {
+        let searchVC = SearchVC() // Replace with your actual view controller
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        return UINavigationController(rootViewController: searchVC)
     }
 
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    // Function to create the favourites navigation controller
+    func createFavouritesNC() -> UINavigationController {
+        let favouritesVC = FavouritesListVC() // Replace with your actual view controller
+        favouritesVC.title = "Favourites"
+        favouritesVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        return UINavigationController(rootViewController: favouritesVC)
     }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
 }
-
--- GHFriends/SearchVC.swift
+++ GHFriends/SearchVC.swift
@@ -6,15 +6,16 @@
 //
 
 import UIKit
+import ScreenGuard
 
 class SearchVC: UIViewController {
 
     let logoImageView = UIImageView()
     let usernameTextField = GHTextField()
-    let callToActionButton =  GHButton(backgroundColor: .systemGreen, title: "Get Followers")
+    let callToActionButton = GHButton(backgroundColor: .systemGreen, title: "Get Followers")
     
     var isUsernameEntered: Bool {
-        return !usernameTextField.text!.isEmpty // returns false
+        return !usernameTextField.text!.isEmpty
     }
     
     override func viewDidLoad() {
@@ -24,32 +25,34 @@ class SearchVC: UIViewController {
         configureTextField()
         configureSearchButton()
         createDismissKeyboardTapGesture()
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        // This will happend everytime the view appears
-        // Most of the time make sure to use super
-        // But if you dont want parent function to be called you dont need super
         super.viewWillAppear(animated)
-        // The next view has a navigation bar  right, so we dont want navigation bar here, so we are hiding it every time the view will appear
-        // hence the name
         navigationController?.isNavigationBarHidden = true
     }
     
-    func createDismissKeyboardTapGesture() {
-        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing)) // end editing causes the view to resign the first responder, what view ? is going to act? right now the keyboard but other tap will end it.
-        view.addGestureRecognizer(tap)
-        
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // Start observing for screenshots
+        (UIApplication.shared.delegate as? AppDelegate)?.screenshotViewModel.startObserving()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        // Stop observing for screenshots when the view disappears
+        (UIApplication.shared.delegate as? AppDelegate)?.screenshotViewModel.stopObserving()
     }
     
-    // anytime you create a function for #selector it has to be an objective c funciton/ since its a remanent of objective c
+    func createDismissKeyboardTapGesture() {
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tap)
+    }
     
     @objc func pushFollowerListVC() {
-        // putitng in guard saying hey, if there is valuye then go ahead and do rest
         guard isUsernameEntered else {
-            presentGHAlertOnMainThread(title: "Empty Username", message: "Please eneter a username, How can you have any pudding!! ?", buttonTitle: "OK")
-             return
+            presentGHAlertOnMainThread(title: "Empty Username", message: "Please enter a username.", buttonTitle: "OK")
+            return
         }
         let followerListVC = FollowerListVC()
         followerListVC.username = usernameTextField.text
@@ -58,60 +61,42 @@ class SearchVC: UIViewController {
     }
     
     func configureLogoImageView() {
-        // This is like you dragging a image view in storyboard
-        // you have to do this on every one
         view.addSubview(logoImageView)
         logoImageView.translatesAutoresizingMaskIntoConstraints = false
-        logoImageView.image = UIImage(named: "gh-logo")! // This is stringly typed (nickname of community name)
-        // Dont use much since it can crash the app
-        // any time you need stringly typed then, make it a constant but here we are using it just once so its fine.
+        logoImageView.image = UIImage(named: "gh-logo")!
         
         NSLayoutConstraint.activate([
-            // just a rule 4 constraint per object, Height, width, x and y
-            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80), // this is like constraint you put in the storybaord
-            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor), // keep it in the center
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             logoImageView.heightAnchor.constraint(equalToConstant: 200),
-            logoImageView.widthAnchor.constraint(equalToConstant: 200) // these are the width and height
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
         ])
     }
     
     func configureTextField() {
         view.addSubview(usernameTextField)
-        // setting the delagate to listen to
-        usernameTextField.delegate = self // self is the SearchVC viewcontroller
+        usernameTextField.delegate = self
         NSLayoutConstraint.activate([
             usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
-            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50), // pining to the edges
-            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50), // always needs to be a negative anchor for trailiing
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
             usernameTextField.heightAnchor.constraint(equalToConstant: 50)
-            
         ])
     }
     
     func configureSearchButton() {
         view.addSubview(callToActionButton)
-        
         callToActionButton.addTarget(self, action: #selector(pushFollowerListVC), for: .touchUpInside)
-        // for is like the differnt types of taps you can do in a button.
-        
         NSLayoutConstraint.activate([
-            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50), // trialing andbottom you have to keep it on negative
+            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
             callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
             callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
             callToActionButton.heightAnchor.constraint(equalToConstant: 50)
-            
         ])
     }
-    
-    
-    
 }
 
-
 extension SearchVC: UITextFieldDelegate {
-    // Delegate listens,listen who ? listen vc
-    // first define the vc or set up
-    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         pushFollowerListVC()
         return true
